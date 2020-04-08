41f2d5086aa7dcf06321c56d782aaa864d7f8e1e
singe <singe-github@singe.za.net>
2020-04-08 23:03:08 +0200

    Better AAAA printf

--- helloworld/AAAA-noTEXT_printf.s
+++ helloworld/AAAA-noTEXT_printf.s
@@ -1,16 +1,15 @@
   .section  __TEXT,__text
   .globl  _main
 _main:
-  subq  $40, %rsp             # Variable space
+  subq  $24, %rsp             # Variable space & stack alignment
 
-  movb  $0, 24(%rsp)          # Null terminator
-  movl  $0x41414141, 20(%rsp) # AAAA
-  movl  $0x7325, 16(%rsp)     # %s
+  movq  $0x41414141, 4(%rsp)  # AAAA
+  movl  $0x7325, (%rsp)       # %s
 
-  leaq  16(%rsp), %rdi        # param 1
-  leaq  20(%rsp), %rsi        # param 2
-  movb  $0, %al
+  leaq  (%rsp), %rdi          # param 1
+  leaq  4(%rsp), %rsi         # param 2
+  movb  $0, %al               # no vector args
   callq _printf               # printf("%s","AAAA")
 
-  addq  $40, %rsp             # Reclaim variable space
+  addq  $24, %rsp             # Reclaim variable space & stack alignment
   retq
